cf561cc57ec5e3a524a83abb3a47dc19daae7c27
Yancharuk Alexander <alex@itvault.info>
2015-09-19 16:52:54 +0300

    Some phpstorm templates fixes. Added wget config

---
+++ phpstorm/.WebIde90/config/fileTemplates/internal/PHP File.php
@@ -0,0 +1,2 @@
+<?php
+#parse("PHP Custom File Header.php")
---
+++ phpstorm/.WebIde90/config/fileTemplates/internal/PHP Trait.php
@@ -0,0 +1,12 @@
+<?php
+#parse("PHP Custom File Header.php")
+
+#if (${NAMESPACE})
+
+namespace ${NAMESPACE};
+
+#end
+
+trait ${NAME} {
+
+}
