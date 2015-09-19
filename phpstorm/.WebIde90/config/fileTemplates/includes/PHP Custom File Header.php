0774e771e1280defde3495316007f84eeef04e29
Yancharuk Alexander <alex@itvault.info>
2015-09-20 01:47:45 +0300

    Update phpstorm templates

--- phpstorm/.WebIde90/config/fileTemplates/includes/PHP Custom File Header.php
+++ phpstorm/.WebIde90/config/fileTemplates/includes/PHP Custom File Header.php
@@ -3,7 +3,9 @@
  *
  * PHP version 5.4+
  *
- * @author  Yancharuk Alexander <alex at itvault dot info>
- * @date    ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
- * @copyright The BSD 3-Clause License
+ * @author    Yancharuk Alexander <alex at itvault dot info>
+ * @copyright © 2012-2015 Alexander Yancharuk <alex at itvault at info>
+ * @date      ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+ * @license   The BSD 3-Clause License
+ *            <http://opensource.org/licenses/BSD-3-Clause>
  */
